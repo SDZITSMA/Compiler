c0aeabe6615f2c5eabb09d7457643fe5f0d5c369
1 <1>
2025-02-04 13:30:32 +0300

    Передайте свой код на сервер

--- Complier/Scanner/Program.cs
+++ Complier/Scanner/Program.cs
@@ -6,7 +6,24 @@ namespace Scanner
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double[] numbers = { 1, -2, 3, 4, -5, 6 };
+
+            double sum = CalculatePositiveSum(numbers);
+
+            Console.WriteLine("Сумма положительных чисел: " + sum);
+        }
+
+        public static double CalculatePositiveSum(double[] arr)
+        {
+            double sum = 0;
+            foreach (double num in arr)
+            {
+                if (num > 0)
+                {
+                    sum += num;
+                }
+            }
+            return sum;
         }
     }
 }
